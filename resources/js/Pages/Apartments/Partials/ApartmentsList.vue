e11f4033910a93d02b01506d0eb4e9e8c9989b22
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-11-18 16:19:43 +0100

    Add no items component

--- resources/js/Pages/Apartments/Partials/ApartmentsList.vue
+++ resources/js/Pages/Apartments/Partials/ApartmentsList.vue
@@ -1,19 +1,23 @@
 <script lang="ts" setup>
 import type { Apartment } from '@/types'
-import ApartmentItem from './ApartmentItem.vue'
+import NoItems from '@/Shared/NoItems.vue'
+import ApartmentItem from '@/Pages/Apartments/Partials/ApartmentItem.vue'
 
 interface Props {
   apartments: Apartment[] | undefined
 }
 
 const props = defineProps<Props>()
+const placeholder: [] = []
 </script>
 
 <template>
   <div class="grid grid-cols-1 gap-24 mb-64">
     <ApartmentItem
+      v-if="props.apartments"
       v-for="apartment in props.apartments"
       :apartment="apartment"
     />
+    <NoItems v-else>Nie masz w tej chwili dodanych domków.</NoItems>
   </div>
 </template>
---
+++ resources/js/Shared/NoItems.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="flex items-center justify-center p-6 bg-slate-200 w-full max-w-3xl mx-auto rounded-md"
+  >
+    <h2 class="text-2xl text-slate-900"><slot /></h2>
+  </div>
+</template>
