3163b4db1be5bfbd9daa4e8af55a7f8f620fdb66
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-11-29 07:55:37 +0100

    Add photoswipe translations

---
+++ resources/js/utils/swipe_pl.ts
@@ -0,0 +1,10 @@
+const swipe_pl = {
+  closeTitle: 'Zamknij',
+  zoomTitle: 'Zbliżenie',
+  arrowPrevTitle: 'Poprzednie',
+  arrowNextTitle: 'Następne',
+  errorMsg: 'Nie można wczytać tego zdjęcia',
+  indexIndicatorSep: ' z ',
+}
+
+export default swipe_pl
