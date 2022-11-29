0309ff2893542d056cdf44d93d51a5abb9e673d9
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-11-29 09:09:43 +0100

    add dark classes to no items

--- resources/js/shared/NoItems.vue
+++ resources/js/shared/NoItems.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div
-    class="flex items-center justify-center p-6 bg-slate-200 w-full max-w-3xl mx-auto rounded-md"
+    class="flex items-center justify-center p-6 bg-slate-200 dark:bg-slate-700 w-full max-w-3xl mx-auto rounded-md"
   >
-    <h2 class="text-2xl text-slate-900"><slot /></h2>
+    <h2 class="text-2xl text-slate-900 dark:text-slate-100"><slot /></h2>
   </div>
 </template>
