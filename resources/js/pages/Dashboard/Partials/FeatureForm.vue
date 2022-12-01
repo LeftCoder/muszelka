eb0ca929aa5e86f5805110c37276f42604d7a53d
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-12-01 09:45:32 +0100

    form processing indicator

--- resources/js/pages/Dashboard/Partials/FeatureForm.vue
+++ resources/js/pages/Dashboard/Partials/FeatureForm.vue
@@ -58,7 +58,7 @@ const onSubmit = () => {
       </Link>
       <button
         type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-white inline-flex bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         :disabled="form.processing"
       >
         <LoadingSpinnerIcon v-if="form.processing" />
