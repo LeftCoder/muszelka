d197d89bad4d258dc64d68059b93c6d6e257cfed
Kamil Graczyk <k.d.graczyk@gmail.com>
2022-12-09 08:42:09 +0100

    fix default tests, config phpunit

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -13,7 +13,7 @@ class AuthenticationTest extends TestCase
 
     public function test_login_screen_can_be_rendered()
     {
-        $response = $this->get('/login');
+        $response = $this->get('/admin');
 
         $response->assertStatus(200);
     }
