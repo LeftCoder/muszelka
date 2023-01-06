9f88bc726eee2e73291af43d64fb7c45d121168b
Kamil Graczyk <k.d.graczyk@gmail.com>
2023-01-07 00:27:21 +0100

    change hover effect

--- resources/js/shared/ImageGallery.vue
+++ resources/js/shared/ImageGallery.vue
@@ -55,10 +55,24 @@ const openGallery = () => {
     >
       {{ props.images?.length ?? 0 }}
     </div>
-    <div
-      class="absolute inset-0 z-10 flex transition hover:opacity-0 hover:cursor-pointer"
-    >
-      <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="absolute inset-0 z-10 flex hover:cursor-pointer">
+      <div
+        class="flex justify-center items-center absolute transition inset-0 opacity-0 hover:opacity-100"
+      >
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#FFF"
+          class="w-24 h-24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6"
+          />
+        </svg>
+      </div>
     </div>
     <img
       v-if="props.images"
